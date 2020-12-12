fdedc2217fba9c7362de07ba870291d4280a883f
fnanni-0 <fnanni@itba.edu.ar>
2020-12-11 22:16:48 -0300

    feat: add Withdrawal event + minor text fixes

--- contracts/0.7.x/interfaces/IAppealEvents.sol
+++ contracts/0.7.x/interfaces/IAppealEvents.sol
@@ -11,20 +11,28 @@ pragma solidity >=0.7;
 interface IAppealEvents {
 
     /** @dev To be emitted when the appeal fees of one of the parties are fully funded.
-     *  @param _itemID The ID of the respective transaction.
-     *  @param _ruling The ruling that is fully funded.
-     *  @param _round The appeal round fully funded by _party. Starts from 0.
+     *  @param _itemID The ID of the respective dispute.
+     *  @param _ruling Indicates the ruling option which got fully funded.
+     *  @param _round The appeal round fully funded by _ruling. Starts from 0.
      */
-    event HasPaidAppealFee(uint256 indexed _itemID, uint256 _ruling, uint256 _round);
+    event HasPaidAppealFee(uint256 indexed _itemID, uint256 indexed _ruling, uint256 _round);
 
     /**
      *  @dev To be emitted when someone contributes to the appeal process.
-     *  @param _itemID The ID of the respective transaction.
-     *  @param _ruling The ruling which received the contribution.
-     *  @param _contributor The address of the contributor.
+     *  @param _itemID The ID of the respective dispute.
+     *  @param _ruling Indicates the ruling option which got the contribution.
+     *  @param _contributor Caller of fundAppeal function.
      *  @param _round The appeal round to which the contribution is going. Starts from 0.
      *  @param _amount The amount contributed.
      */
-    event AppealContribution(uint256 indexed _itemID, uint256 _ruling, address indexed _contributor, uint256 _round, uint256 _amount);
+    event AppealContribution(uint256 indexed _itemID, uint256 indexed _ruling, address indexed _contributor, uint256 _round, uint256 _amount);
 
+    /** @dev To be raised inside withdrawFeesAndRewards function.
+     *  @param _itemID The ID of the respective dispute.
+     *  @param _round The appeal round to which the contribution is going. Starts from 0.
+     *  @param _ruling Indicates the ruling option which got the contribution.
+     *  @param _contributor Caller of fundAppeal function.
+     *  @param _reward Total amount of deposits reimbursed plus rewards.
+     */
+    event Withdrawal(uint256 indexed _itemID, uint256 indexed _round, uint256 _ruling, address indexed _contributor, uint256 _reward);
 }
